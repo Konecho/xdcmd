f0c8cf438bd7d0540cf58983520f59fd2dd41685
暴雪王 <konecho@outlook.com>
2025-01-17 15:59:24 +0800

    test

--- update.py
+++ update.py
@@ -4,7 +4,11 @@ from pathlib import Path
 
 src = Path("_sources/generated.json").open()
 ver = json.load(src)["xdcmd"]["version"]
-
 env_file = Path(os.getenv("GITHUB_ENV"))
-if env_file.exists():
+# env_file = Path("GITHUB_ENV") # for test
+
+old_ver = os.environ.get("OLD_VERSION")
+if old_ver is None:
+    env_file.open("a").write(f'OLD_VERSION="{ver}"')
+elif old_ver != ver:
     env_file.open("a").write(f'COMMIT_MSG="auto update: {ver}"')
